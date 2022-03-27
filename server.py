6cb6263dda1fa249f0444b9e32f20a3674ab7cb1
Elijah Haley <elijahhaley99@gmail.com>
2022-03-27 13:09:18 -0700

    python

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+@app.route('/play/<int:times>')
+def play(times):
+    return render_template('index.html' , times = times)
+
+@app.route('/play/<int:times>/<string:color>')
+def color(times,color):
+    return render_template('index.html' , times = times , color = color)
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
